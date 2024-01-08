452d175716ed8aa7a2f03ef386f827cc88d5b349
Min <solunar@vip.qq.com>
2024-01-08 13:20:20 +0800

    Update

--- src/components/dialog/index.vue
+++ src/components/dialog/index.vue
@@ -1,16 +1,30 @@
 <script lang="ts" setup>
+import { reactive, ref } from 'vue';
 import { tooltipJumpUp } from '@/components';
 
 defineOptions({ name: 'c-dialog' });
 defineProps<{ title: string }>();
 defineEmits(['cancel', 'confirm']);
+
+const cardRef = ref();
+const position = reactive({ left: 'auto', top: 'auto', x: 0, y: 0 });
+
+const handlePan = (ev: any) => {
+  if (ev.isFirst) {
+    position.x = ev.position.left - cardRef.value.$el.offsetLeft;
+    position.y = ev.position.top - cardRef.value.$el.offsetTop;
+  }
+  position.left = `${ev.position.left - position.x}px`;
+  position.top = `${ev.position.top - position.y}px`;
+};
 </script>
 
 <template>
   <q-dialog v-bind="$attrs">
-    <q-card>
-      <div class="bg-gray-100 flex justify-between items-center">
-        <div class="pl-2">{{ $props.title }}</div>
+    <q-card :style="{ left: position.left, top: position.top }" ref="cardRef" class="absolute">
+      <div class="bg-gray-100 flex items-center">
+        <div class="px-2">{{ $props.title }}</div>
+        <q-space v-touch-pan.prevent.mouse="handlePan" class="self-stretch cursor-grab" />
         <q-btn color="negative" icon="close" padding="sm" size="sm" flat v-close-popup>
           <q-tooltip v-bind="tooltipJumpUp">Close</q-tooltip>
         </q-btn>
