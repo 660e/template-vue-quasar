0d7c00bd1f1aad34f6ec34e4dcb4af53f5fa2272
Min <solunar@vip.qq.com>
2024-01-10 16:26:05 +0800

    Update

--- src/components/table/index.vue
+++ src/components/table/index.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, useAttrs } from 'vue';
 import { QTableColumn } from 'quasar';
 
 defineOptions({ name: 'c-table' });
+
+const $attrs = useAttrs();
 
 const CTableRef = ref();
 
 const height = ref('auto');
 const width = ref('auto');
+
+const columns = $attrs.columns as QTableColumn[];
+const visibleColumns = ref(columns.filter(column => !column.required).map(column => column.label));
 
 onMounted(() => {
   height.value = `${CTableRef.value.$el.clientHeight}px`;
@@ -20,12 +25,29 @@ onMounted(() => {
     v-bind="$attrs"
     :rows-per-page-options="[20, 50, 100]"
     :style="{ height, width }"
-    :class="{ 'c-table-handle': ($attrs.columns as QTableColumn[]).map(column => column.name).includes('handle') }"
+    :class="{ 'c-table-handle': columns.map(column => column.name).includes('handle') }"
+    :visible-columns="visibleColumns"
     class="c-table"
     ref="CTableRef"
     bordered
     flat
   >
+    <template v-slot:top>
+      <q-space />
+      <q-select
+        v-model="visibleColumns"
+        :options="columns.filter(column => !column.required)"
+        :display-value="$q.lang.table.columns"
+        option-value="label"
+        dense
+        emit-value
+        map-options
+        multiple
+        options-dense
+        outlined
+      />
+    </template>
+
     <template v-slot:loading>
       <q-inner-loading class="z-10" color="primary" showing />
     </template>
