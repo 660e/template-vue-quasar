6856d0249e7415836b2cbc32753f034f91d0f8ec
Min <solunar@vip.qq.com>
2024-02-24 11:02:09 +0800

    Update

--- src/layouts/components/app-header.vue
+++ src/layouts/components/app-header.vue
@@ -3,12 +3,12 @@ import $package from '../../../package.json';
 </script>
 
 <template>
-  <q-header>
-    <q-toolbar>
-      <q-toolbar-title>{{ $package.productName }}</q-toolbar-title>
-      <q-btn :icon="$q.dark.isActive ? 'brightness_5' : 'brightness_4'" @click="$q.dark.toggle()" dense flat round>
-        <q-tooltip>{{ $q.dark.isActive ? 'Light' : 'Dark' }}</q-tooltip>
-      </q-btn>
-    </q-toolbar>
+  <q-header class="h-12 px-4 flex items-center">
+    <div class="flex-1">
+      <span class="text-xl">{{ $package.productName }}</span>
+    </div>
+    <q-btn :icon="$q.dark.isActive ? 'brightness_5' : 'brightness_4'" @click="$q.dark.toggle()" dense flat round>
+      <q-tooltip>{{ $q.dark.isActive ? 'Light' : 'Dark' }}</q-tooltip>
+    </q-btn>
   </q-header>
 </template>
