f7bbb4b0fd005a6a502e7e27b8c3d15226c0b0b7
Min <solunar@vip.qq.com>
2024-02-25 22:05:30 +0800

    Update

---
+++ src/layouts/components/app-trigger.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { useLayoutStore } from '@/stores/modules/layout';
+
+const $layoutStore = useLayoutStore();
+const toggle = () => {
+  $layoutStore.drawer = !$layoutStore.drawer;
+  window.dispatchEvent(new Event('resize'));
+};
+</script>
+
+<template>
+  <div
+    @click="toggle"
+    class="absolute top-1/2 left-0 -translate-y-1/2 h-20 w-4 flex flex-col justify-center items-center cursor-pointer group bg-red-100"
+  >
+    <div
+      :class="[`group-hover:${$layoutStore.drawer ? '' : '-'}rotate-12`]"
+      class="h-3 w-1 rounded-full bg-gray-500 translate-y-0.5 duration-300"
+    ></div>
+    <div
+      :class="[`group-hover:${$layoutStore.drawer ? '-' : ''}rotate-12`]"
+      class="h-3 w-1 rounded-full bg-gray-500 -translate-y-0.5 duration-300"
+    ></div>
+  </div>
+</template>
--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,21 +1,18 @@
 <script lang="ts" setup>
-import { ref } from 'vue';
 import { routes } from '@/router';
 import { RouteRecordRaw } from 'vue-router';
+import { useLayoutStore } from '@/stores/modules/layout';
 
 import AppHeader from '@/layouts/components/app-header.vue';
 import AppFooter from '@/layouts/components/app-footer.vue';
 import AppTabs from '@/layouts/components/app-tabs.vue';
+import AppTrigger from '@/layouts/components/app-trigger.vue';
 
 import MenuComponent from './components/menu.vue';
 
 defineOptions({ name: 'app-layout' });
 
-const drawer = ref(true);
-const toggle = () => {
-  drawer.value = !drawer.value;
-  window.dispatchEvent(new Event('resize'));
-};
+const $layoutStore = useLayoutStore();
 
 const tweak = (offset: number, height: number) => ({ height: `${height - offset - (36 + 1) * 2}px` });
 
@@ -34,19 +31,13 @@ const include: string[] = filterKeepAlive(routes);
 <template>
   <q-layout view="hhh lpr fff" class="overflow-hidden">
     <app-header />
-    <q-drawer v-model="drawer" :width="200" bordered>
+    <q-drawer v-model="$layoutStore.drawer" :width="200" bordered>
       <menu-component />
     </q-drawer>
     <q-page-container>
       <app-tabs />
       <q-page :style-fn="tweak" class="overflow-y-auto overflow-x-hidden relative">
-        <div
-          @click="toggle"
-          class="absolute top-1/2 left-0 -translate-y-1/2 h-20 w-4 flex flex-col justify-center items-center cursor-pointer group bg-red-100"
-        >
-          <div :class="[`group-hover:${drawer ? '' : '-'}rotate-12`]" class="h-3 w-1 rounded-full bg-gray-500 translate-y-0.5 duration-300"></div>
-          <div :class="[`group-hover:${drawer ? '-' : ''}rotate-12`]" class="h-3 w-1 rounded-full bg-gray-500 -translate-y-0.5 duration-300"></div>
-        </div>
+        <app-trigger />
         <router-view v-slot="{ Component }">
           <transition enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" mode="out-in">
             <keep-alive :exclude="exclude" :include="include">
