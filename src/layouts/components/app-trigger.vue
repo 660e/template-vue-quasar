6a9df4c7815169e2ed60a212860972c201c08849
Min <solunar@vip.qq.com>
2024-02-25 22:31:22 +0800

    Update

--- src/layouts/components/app-trigger.vue
+++ src/layouts/components/app-trigger.vue
@@ -9,17 +9,14 @@ const toggle = () => {
 </script>
 
 <template>
-  <div
-    @click="toggle"
-    class="absolute top-1/2 left-0 -translate-y-1/2 h-20 w-4 flex flex-col justify-center items-center cursor-pointer group bg-red-100"
-  >
-    <div
-      :class="[`group-hover:${$layoutStore.drawer ? '' : '-'}rotate-12`]"
-      class="h-3 w-1 rounded-full bg-gray-500 translate-y-0.5 duration-300"
-    ></div>
-    <div
-      :class="[`group-hover:${$layoutStore.drawer ? '-' : ''}rotate-12`]"
-      class="h-3 w-1 rounded-full bg-gray-500 -translate-y-0.5 duration-300"
-    ></div>
+  <div @click="toggle" class="absolute top-1/2 left-0 -translate-y-1/2 h-20 w-4 flex flex-col justify-center items-center cursor-pointer group">
+    <template v-if="$layoutStore.drawer">
+      <div class="h-3 w-1 rounded-full bg-gray-500 translate-y-0.5 duration-300 group-hover:rotate-12"></div>
+      <div class="h-3 w-1 rounded-full bg-gray-500 -translate-y-0.5 duration-300 group-hover:-rotate-12"></div>
+    </template>
+    <template v-else>
+      <div class="h-3 w-1 rounded-full bg-gray-500 translate-y-0.5 duration-300 group-hover:-rotate-12"></div>
+      <div class="h-3 w-1 rounded-full bg-gray-500 -translate-y-0.5 duration-300 group-hover:rotate-12"></div>
+    </template>
   </div>
 </template>
