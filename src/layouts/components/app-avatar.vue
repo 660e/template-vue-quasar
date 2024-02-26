d45b2f4427d45e202fd7a860e9401a33b81d7fba
Min <solunar@vip.qq.com>
2024-02-26 21:04:59 +0800

    Update

---
+++ src/layouts/components/app-avatar.vue
@@ -0,0 +1,7 @@
+<script lang="ts" setup></script>
+
+<template>
+  <q-avatar size="36px">
+    <img src="https://avatars.githubusercontent.com/u/28943787" />
+  </q-avatar>
+</template>
--- src/layouts/components/app-header.vue
+++ src/layouts/components/app-header.vue
@@ -2,13 +2,15 @@
 import $package from '../../../package.json';
 
 import AppAppearance from '@/layouts/components/app-appearance.vue';
+import AppAvatar from '@/layouts/components/app-avatar.vue';
 </script>
 
 <template>
-  <q-header class="h-12 px-4 flex items-center">
+  <q-header class="h-12 px-4 flex items-center space-x-1">
     <div class="flex-1">
       <span class="text-xl">{{ $package.productName }}</span>
     </div>
     <app-appearance />
+    <app-avatar />
   </q-header>
 </template>
